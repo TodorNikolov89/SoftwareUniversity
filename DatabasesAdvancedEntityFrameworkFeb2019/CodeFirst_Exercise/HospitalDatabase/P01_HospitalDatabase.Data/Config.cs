915a0a178e6b34df85dbed58adc8542e8704191b
Todor <tod_ant@yahoo.com>
2019-03-07 01:00:53 +0200

    Added HospitalDatabase

---
+++ DatabasesAdvancedEntityFrameworkFeb2019/CodeFirst_Exercise/HospitalDatabase/P01_HospitalDatabase.Data/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace P01_HospitalDatabase.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Config
+    {
+        public const string ConnectionString = @"Server=TodorsPC\SQLEXPRESS;Database=Hospital;Integrated Security=True;";
+    }
+}
