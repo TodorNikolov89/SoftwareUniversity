0f47fcbd49587d415c1d7997efbab42bd2353079
Todor <tod_ant@yahoo.com>
2020-04-19 11:28:23 +0300

    Refactoring

--- ProgrammingBasics_JavaScript/ConditionalStatementsAdvanced_Exercise/hotelRoom.js
+++ ProgrammingBasics_JavaScript/ConditionalStatementsAdvanced_Exercise/hotelRoom.js
@@ -2,42 +2,41 @@ function hotelRoom(month, nights) {
     nights = Number(nights)
     let studioPrice = 0;
     let apartmentPrice = 0;
-    let studioDiscount = 0;
-    let apartmentDiscount = 0;
     let studioMoney = 0;
     let apartmentMoney = 0
 
-    if (month === 'May' || month === 'Octomber') {
-        studioPrice = 50;
-        apartmentPrice = 65;
+    if (month == 'May' || month == 'October') {
+        studioPrice = 50.0;
+        apartmentPrice = 65.0;
 
         if (nights > 14) {
-            studioPrice *= 1-0.3;
+            studioPrice *= 0.7;
         } else if (nights > 7) {
-            studioPrice *= 1-0.05;
+            studioPrice *= 0.95;
         }
-    } else if (month === 'June' || month === 'September') {
+    } else if (month == 'June' || month == 'September') {
         studioPrice = 75.20;
         apartmentPrice = 68.70;
 
         if (nights > 14) {
-            studioPrice *= 1-0.2;
+            studioPrice *= 0.8;
         }
        
-    } else if (month === 'July' || month === 'August') {
-        studioPrice = 76;
-        apartmentPrice = 77;
+    } else if (month == 'July' || month == 'August') {
+        studioPrice = 76.0;
+        apartmentPrice = 77.0;        
     }
 
     if (nights > 14) {
-        apartmentPrice *=1-0.1;
+        apartmentPrice *= 0.90;
     }
 
     studioMoney = studioPrice * nights;
 
     apartmentMoney = apartmentPrice * nights;
 
-    console.log(`Apartment: ${apartmentMoney.toFixed(2)} lv.\nStudio: ${studioMoney.toFixed(2)} lv.`)
+    console.log(`Apartment: ${apartmentMoney.toFixed(2)} lv.`)
+    console.log(`Studio: ${studioMoney.toFixed(2)} lv.`)
 }
 
 // hotelRoom('August', 20)
