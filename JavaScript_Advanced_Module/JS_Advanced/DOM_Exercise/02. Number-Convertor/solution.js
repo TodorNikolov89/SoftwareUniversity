750bf7642a7dda7efdc70485de29e5a5935596f5
Todor <tod_ant@yahoo.com>
2020-05-19 23:58:53 +0300

    Added solution for Number-Convertor

--- JavaScript_Advanced_Module/JS_Advanced/DOM_Exercise/02. Number-Convertor/solution.js
+++ JavaScript_Advanced_Module/JS_Advanced/DOM_Exercise/02. Number-Convertor/solution.js
@@ -1,4 +1,19 @@
 function solve() {
+    let optionList = document.querySelectorAll("#selectMenuTo")[0];
+    let convertButton = document.querySelector('button');
+    let num = document.querySelector('#input');
 
-    //TODO...
+    let mathObj = {
+        binary: (number) => number.toString(2),
+        hexadecimal: (number) => number.toString(16).toUpperCase(),
+    }
+
+    optionList.innerHTML = `    
+    <option selected value=""></option>
+    <option value="hexadecimal">Hexadecimal</option>
+    <option value="binary">Binary</option>
+    `
+    convertButton.addEventListener('click', () => {
+        document.getElementById('result').value = mathObj[optionList.value](Number(num.value))
+    })
 }
