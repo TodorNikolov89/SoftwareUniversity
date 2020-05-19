f5a639bcf30fc6a94269a6952de3b42de39f40ae
Todor <tod_ant@yahoo.com>
2020-05-19 22:12:07 +0300

    Added Solution for Chat Room in JS_Advanced

--- JavaScript_Advanced_Module/JS_Advanced/DOM_Exercise/01. Chat-Room/solution.js
+++ JavaScript_Advanced_Module/JS_Advanced/DOM_Exercise/01. Chat-Room/solution.js
@@ -1,5 +1,15 @@
 function solve() {
-   //TODO...
+   let sendButton = document.getElementById("send");
+
+   sendButton.addEventListener('click', function () {
+      let textAreaContent = document.getElementById("chat_input").value;
+      let newMessage = document.createElement("div");
+      newMessage.className = "message my-message";
+      newMessage.textContent = textAreaContent;
+      newMessage.style.wordBreak="break-all"
+
+      let messageBox = document.getElementById("chat_messages");
+      messageBox.appendChild(newMessage);
+      document.getElementById("chat_input").value = "";
+   })
 }
-
-
