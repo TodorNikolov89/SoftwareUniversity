9579bfbea2ea39ea0fe945d180ed6426fa4fafd5
Todor <tod_ant@yahoo.com>
2020-05-22 14:33:55 +0300

    Order-the-names solution.js is added

--- JavaScript_Advanced_Module/JS_Advanced/DOM_Exercise/05. Order-the-Names/solution.js
+++ JavaScript_Advanced_Module/JS_Advanced/DOM_Exercise/05. Order-the-Names/solution.js
@@ -1,4 +1,21 @@
 function solve() {
+    let addButton = document.getElementsByTagName('button')[0]
+    let name = document.getElementsByTagName('input')[0].value;
+    addButton.addEventListener('click', () => {        
+        let ch = name[0].toUpperCase();
+        name = name.slice(1, name.length).toLowerCase()
+        let i = ch.charCodeAt(0)
+        let row = i - 65;
+        let list = document.getElementsByTagName("ol")[0];
+        let currentContent = list.children[row].innerHTML;
+        name = ch + name;
+        if (currentContent === "") {
+            currentContent += name;
+        } else {
+            currentContent += `, ${name}`;
+        }
 
-    //TODO...
+        list.children[row].innerHTML = currentContent;
+        document.getElementsByTagName('input')[0].value = "";
+    })
 }
