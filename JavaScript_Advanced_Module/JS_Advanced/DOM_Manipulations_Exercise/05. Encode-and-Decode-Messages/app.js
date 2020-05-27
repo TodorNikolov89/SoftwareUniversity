d5e52e92474a7b4b382c3ee748a3b7a9aad85be2
Todor <tod_ant@yahoo.com>
2020-05-27 22:36:15 +0300

    Added Solutions for encodeAndDecodeMessages

--- JavaScript_Advanced_Module/JS_Advanced/DOM_Manipulations_Exercise/05. Encode-and-Decode-Messages/app.js
+++ JavaScript_Advanced_Module/JS_Advanced/DOM_Manipulations_Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,34 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeBtn = document.getElementsByTagName('button')[0]
+    let decodeBtn = document.getElementsByTagName('button')[1]
+    let senderTextArea = document.querySelectorAll('textarea')[0]
+    let receiverTextArea = document.querySelectorAll('textarea')[1]
+
+    encodeBtn.addEventListener('click', () => {
+        let message = senderTextArea.value;
+        let encodedMessage = '';
+
+        for (let i = 0; i < message.length; i++) {
+            encodedMessage += String.fromCharCode(getAscii(message[i]) + 1)
+        }
+
+        senderTextArea.value = "";
+        receiverTextArea.value = encodedMessage;
+
+    })
+
+    decodeBtn.addEventListener('click', () => {
+        let message = receiverTextArea.value;
+        let decodedMessage = '';
+
+        for (let i = 0; i < message.length; i++) {
+            decodedMessage += String.fromCharCode(getAscii(message[i]) - 1)
+        }
+
+        receiverTextArea.value = decodedMessage;
+    })
+
+    function getAscii(ch) {
+        return ch.charCodeAt(0)
+    }
 }
