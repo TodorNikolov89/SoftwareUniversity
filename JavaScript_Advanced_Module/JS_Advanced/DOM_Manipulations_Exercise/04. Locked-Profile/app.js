50066d08e00c2ae9901725b3967e4186d01bc898
Todor <tod_ant@yahoo.com>
2020-05-27 21:15:10 +0300

    Added Solutions for 03.Time-Converter and 04.lockedProfile

--- JavaScript_Advanced_Module/JS_Advanced/DOM_Manipulations_Exercise/04. Locked-Profile/app.js
+++ JavaScript_Advanced_Module/JS_Advanced/DOM_Manipulations_Exercise/04. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let btns = document.querySelectorAll('button');
+
+    for (const b of btns) {
+        b.addEventListener('click', showHideInformation)
+    }
+
+    function showHideInformation(event) {
+        let btn = event.target;
+        let profile = btn.parentNode;
+        let lockButton = profile.querySelector('input[type="radio"]:checked').value;
+        let hiddenDiv = profile.querySelectorAll('div')[0];
+
+        if (lockButton === 'unlock') {
+            if (btn.textContent === 'Hide it') {
+                hiddenDiv.style.display = 'none'
+                btn.textContent = 'Show more';
+            } else {
+                btn.textContent = 'Show more'
+                hiddenDiv.style.display = 'block'
+                btn.textContent = 'Hide it';
+            }
+        }
+    }
 }
