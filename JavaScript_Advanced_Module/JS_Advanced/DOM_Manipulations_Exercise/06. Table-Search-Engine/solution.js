83e78165a8a517397e7528334f0ffe3450ce7f06
Todor <tod_ant@yahoo.com>
2020-05-27 23:57:49 +0300

    Added Solutions for Table-Search Engine

--- JavaScript_Advanced_Module/JS_Advanced/DOM_Manipulations_Exercise/06. Table-Search-Engine/solution.js
+++ JavaScript_Advanced_Module/JS_Advanced/DOM_Manipulations_Exercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-   //TODO...
+   let searchBtn = document.querySelector('#searchBtn');
+   let tableBody =Array.from(document.getElementsByTagName('tbody')[0].children);
+   let searchField = document.getElementById('searchField');
+
+   searchBtn.addEventListener('click', function () {
+      let regex = new RegExp(searchField.value, 'gim');
+      tableBody.map(e => {
+         e.classList.remove('select');
+         if (e.innerHTML.match(regex) !== null) {
+            e.className = 'select';
+         }
+      });
+      searchField.value = '';
+   });
 }
