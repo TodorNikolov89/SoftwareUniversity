fbdc6b9dbbeac832c5d309c0afd35ecc6782545d
Todor <tod_ant@yahoo.com>
2020-05-31 21:40:27 +0300

    Added Solution for distanceConverter.js

--- JavaScript_Advanced_Module/JS_Advanced/DOM_Manipulations_Exercise/09. Distance-Converter/distanceConverter.js
+++ JavaScript_Advanced_Module/JS_Advanced/DOM_Manipulations_Exercise/09. Distance-Converter/distanceConverter.js
@@ -1,3 +1,47 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let convertBtn = document.getElementById('convert');
+    convertBtn.addEventListener('click', convert)
+
+    let inputDistance = document.getElementById('inputDistance');
+    let outputDistance = document.getElementById('outputDistance');
+
+    let convertFrom = document.getElementById('inputUnits');
+    let convertTo = document.getElementById('outputUnits');
+
+
+
+    let arr = new Set();
+
+    arr = {
+        'km': 1000,
+        'm': 1,
+        'cm': 0.01,
+        'mm': 0.001,
+        'mi': 1609.34,
+        'yrd': 0.9144,
+        'ft': 0.3048,
+        'in': 0.0254
+    }
+
+    function convert() {
+        let inputValue = Number(inputDistance.value);
+        if (inputValue === '' || inputValue === '') {
+            return;
+        }
+
+        if (isNaN(inputValue)) {
+            return;
+        }
+
+
+        let fromRate = arr[convertFrom.value];
+        let targetRate = arr[convertTo.value];
+        if (!fromRate || !targetRate) {
+            return;
+        }
+
+        let result = inputValue * fromRate / targetRate;
+        outputDistance.value = result;
+
+    }
 }
