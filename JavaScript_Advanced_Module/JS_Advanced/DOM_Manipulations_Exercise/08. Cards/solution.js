fdd6de07c786c6f8be0146cead27a417b2c43a0d
Todor <tod_ant@yahoo.com>
2020-05-31 20:42:09 +0300

    Added Solution for Cards in JS_Advanced/DOM_Manipulations_Exercise/JS-Advanced-DOM-Manipulations-Exercise

--- JavaScript_Advanced_Module/JS_Advanced/DOM_Manipulations_Exercise/08. Cards/solution.js
+++ JavaScript_Advanced_Module/JS_Advanced/DOM_Manipulations_Exercise/08. Cards/solution.js
@@ -1,3 +1,48 @@
 function solve() {
-   // TODO...
+   let allCards = document.querySelectorAll('img');
+   let resultDiv = document.getElementById('result');
+   let history = document.getElementById('history');
+   let leftSpan = resultDiv.children[0];
+   let rightSpan = resultDiv.children[2];
+   let counter = 0;
+   let firstPlayer;
+   let secondPlyer;
+   let resultHTML = [];
+
+   Array.from(allCards).forEach(element => {
+      element.addEventListener('click', () => {
+         let hand = '';
+         element.src = "images/whiteCard.jpg";
+         counter++;
+
+         if (element.parentElement.id === 'player1Div') {
+            firstPlayer = element;
+            leftSpan.textContent = element.name;
+         } else {
+            rightSpan.textContent = element.name;
+            secondPlyer = element;
+         }
+
+         if (counter === 2) {
+            let leftNumber = Number(leftSpan.textContent)
+            let rightNumber = Number(rightSpan.textContent)
+            if (leftNumber > rightNumber) {
+               firstPlayer.style.border = "2px solid green";
+               secondPlyer.style.border = "2px solid red";
+            } else {
+               secondPlyer.style.border = "2px solid green";
+               firstPlayer.style.border = "2px solid red";
+            }
+
+            hand = `[${firstPlayer.name} vs ${secondPlyer.name}]`
+            resultHTML.push(hand);
+
+            history.textContent = resultHTML.join(' ') + ' '
+            leftSpan.textContent = "";
+            rightSpan.textContent = "";
+            
+            counter = 0;
+         }
+      })
+   });
 }
