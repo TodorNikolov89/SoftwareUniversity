790a08ae701893137fe89c63838098274788e902
Todor <tod_ant@yahoo.com>
2020-06-01 20:34:03 +0300

    Added Solution in ObjectAndClasses_Exercise for tickets problem

--- JavaScript_Advanced_Module/JS_Advanced/ObjectAndClasses_Exercise/tickets.js
+++ JavaScript_Advanced_Module/JS_Advanced/ObjectAndClasses_Exercise/tickets.js
@@ -1,31 +1,42 @@
-class Ticket {
-    constructor(destination, price, status) {
-        this.destination = destination;
-        this.price = Number(price);
-        this.status = status;
+function solve(tickets, criteria) {
+    class Ticket {
+        constructor(destination, price, status) {
+            this.destination = destination;
+            this.price = Number(price);
+            this.status = status;
+        }
+    }
+    let result = []
+
+    for (let i = 0; i < tickets.length; i++) {
+        let currentTicket = tickets[i].split("|");
+        let newTicket = new Ticket(currentTicket[0], currentTicket[1], currentTicket[2]);
+        result.push(newTicket);
+    }
+    
+    return result.sort(compare)
+
+    function compare(a, b) {
+        if (a[criteria] < b[criteria]) {
+            return -1;
+        }
+        if (a[criteria] > b[criteria]) {
+            return 1;
+        }
+        return 0;
     }
 }
 
+// solve(['Philadelphia|94.20|available',
+//     'New York City|95.99|available',
+//     'New York City|95.99|sold',
+//     'Boston|126.20|departed'],
+//     'status'
+// )
 
-function solve(towns, criteria) {
-    let result = []
-    towns.forEach(ticket => {
-        let currentTicket = ticket.split("|");
-        let newTicket = new Ticket(currentTicket[0], currentTicket[1], currentTicket[2]);
-        result.push(newTicket);
-    });
-
-
-    console.log(result)
-}
-
-
-
-solve([
-    'Philadelphia|94.20|available',
+console.log(solve(['Philadelphia|94.20|available',
     'New York City|95.99|available',
     'New York City|95.99|sold',
     'Boston|126.20|departed'],
     'destination'
-)
-
+))
