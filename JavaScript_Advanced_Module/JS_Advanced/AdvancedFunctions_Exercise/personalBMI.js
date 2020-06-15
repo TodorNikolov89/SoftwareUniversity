eb0c9a8e3fb54665ea1a13769850e2d1dc72704f
Todor <tod_ant@yahoo.com>
2020-06-15 19:22:37 +0300

    Added solutions for AdvancedFunctions_Exercise

---
+++ JavaScript_Advanced_Module/JS_Advanced/AdvancedFunctions_Exercise/personalBMI.js
@@ -0,0 +1,21 @@
+function solve(name, age, weight, height) {
+    let person = {
+        name: name,
+        personalInfo: {
+            age: age,
+            weight: weight,
+            height: height,
+        },
+        BMI: Math.round(weight / (height * height / 10000))
+    };
+
+    person.status = person.BMI < 18.5 ? "underweight" : person.BMI < 25 ? "normal" : person.BMI < 30 ? "overweight" : "obese";
+
+    if (person.status === 'obese') {
+        person.recommendation = 'admission required';
+    }
+
+    return person;
+}
+
+console.log(solve('Honey Boo Boo', 9, 57, 137))
