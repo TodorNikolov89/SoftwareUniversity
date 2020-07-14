55d417c95a2164c38dcb2106cb19d1618869a4f8
Todor <tod_ant@yahoo.com>
2020-07-14 16:04:23 +0300

    Added changes

--- JS_Applications/Asynchronous Programming_Lab/01.GITHUB-COMMITS/app.js
+++ JS_Applications/Asynchronous Programming_Lab/01.GITHUB-COMMITS/app.js
@@ -1,4 +1,28 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let username = document.querySelector('input#username').value;
+    let repository = document.querySelector('input#repo').value;
+    let commits = document.querySelector('#commits')
+    commits.innerHTML = ''
+    let url = `https://api.github.com/repos/${username}/${repository}/commits`;
+
+    fetch(url)
+        .then((responce) => {
+            if (!responce.ok) {
+                throw new Error(`${responce.status} (${responce.statusText})`)
+            }
+
+            return responce.json()
+        })
+        .then((responce) => {
+            for (const c of responce) {
+                let liElement = document.createElement('li');
+                liElement.textContent = `${c.commit.author.name}: ${c.commit.message}`
+                commits.appendChild(liElement)
+            }
+        })
+        .catch((e) => {
+            let liElement = document.createElement('li');
+            liElement.textContent = `${e}`
+            commits.appendChild(liElement)
+        })
 }
