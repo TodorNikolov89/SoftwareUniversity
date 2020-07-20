9f6a5432edc22a17b9208faac875fde4c4527a53
Todor <tod_ant@yahoo.com>
2020-07-20 15:43:30 +0300

    Added resources

---
+++ JS_Applications/Templating_Exercise/01. List Towns/app.js
@@ -0,0 +1,25 @@
+/*globals Handlebars*/
+window.addEventListener('load', () => {
+    const templateString = document.getElementById('main-template').innerHTML;
+    Handlebars.registerPartial('town', document.getElementById('town-template').innerHTML);
+
+    const templateFn = Handlebars.compile(templateString);
+
+    document.querySelector('#btnLoadTowns').addEventListener('click', renderTowns);
+
+    const input = document.querySelector('#towns');
+    const rootEl = document.querySelector('#root');
+
+    function renderTowns(e) {
+        e.preventDefault();
+        const towns = input.value.split(', ');
+        const generatedHtml = templateFn({ towns });
+        rootEl.innerHTML = generatedHtml;
+    }
+
+    async function getData(url) {
+        let result = await fetch(url);
+        let data = await result.text()
+        return data;
+    }
+})
